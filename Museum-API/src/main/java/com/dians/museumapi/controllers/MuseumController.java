e6c80cc882d01cc3483f7ae96624cee0586bafd3
Vedran <vedrandeskovski5@gmail.com>
2023-11-11 00:09:15 +0100

    adding base html

--- Museum-API/src/main/java/com/dians/museumapi/controllers/MuseumController.java
+++ Museum-API/src/main/java/com/dians/museumapi/controllers/MuseumController.java
@@ -3,27 +3,34 @@ package com.dians.museumapi.controllers;
 import com.dians.museumapi.models.Museum;
 import com.dians.museumapi.repositories.MuseumRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/museums")
+//@RestController("/museums")
+@Controller
+@RequestMapping("/museums")
 public class MuseumController {
-    /// ahsgfvhsavf sadkjfbf 123213123
     @Autowired
     private MuseumRepo museumRepo;
-
-    @GetMapping(path = "")
-    public List<Museum> getAllMuseums() {
-
-        return museumRepo.findAll();
+    @GetMapping()
+    public String getHomePage(Model model){
+        model.addAttribute("museumList", museumRepo.findAll());
+        return "index";
     }
 
-    @GetMapping(path = "/museums/{id}")
-    public Museum getMuseumById(@PathVariable("id") Long id) {
-
-        return museumRepo.findById(id).orElseThrow();
+    @GetMapping("/detailedMuseum/{id}")
+    public String getDetailedMuseum(@PathVariable Long id, Model model){
+        if (museumRepo.findById(id).isPresent()){
+            Museum museum = museumRepo.findById(id).get();
+            model.addAttribute("museum", museum);
+            return "detailsMuseum";
+        }
+        return "redirect:/museums";
     }
 }
