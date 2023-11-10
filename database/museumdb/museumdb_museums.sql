87c0880118a06b5aad5ed189211f6bee690b3327
Vedran <vedrandeskovski5@gmail.com>
2023-11-11 00:18:50 +0100

    update database

--- database/museumdb/museumdb_museums.sql
+++ database/museumdb/museumdb_museums.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: museumdb
+-- Host: localhost    Database: museumdb
 -- ------------------------------------------------------
 -- Server version	8.0.35
 
@@ -42,7 +42,7 @@ CREATE TABLE `museums` (
 
 LOCK TABLES `museums` WRITE;
 /*!40000 ALTER TABLE `museums` DISABLE KEYS */;
-INSERT INTO `museums` VALUES (1,'Contemporary Art Museum of Macedonia','Skopje','Samoilova','museum@gmail.com',NULL,NULL,NULL,NULL),(2,'Museum of Macedonia','Skopje','Prohor Pcinski','museum@gmail.com',NULL,NULL,NULL,NULL);
+INSERT INTO `museums` VALUES (1,'Contemporary Art Museum of Macedonia','Skopje','Samoilova','msu-info@msuskopje.org.mk','Tu-Sa 10:00-15:00; Su 09:00-13:00','+389 2 3117734','https://msu.mk/home/','https://www.openstreetmap.org/way/32887087'),(2,'Museum of Macedonia','Skopje','Prohor Pcinski',NULL,NULL,NULL,NULL,'https://www.openstreetmap.org/way/32812963');
 /*!40000 ALTER TABLE `museums` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-10 21:01:00
+-- Dump completed on 2023-11-11  0:17:06
