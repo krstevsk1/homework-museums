26b2ee9343f4f158461236550670ee0e5b837f93
David Krstevski <david.krstevski@students.finki.ukim.mk>
2023-12-24 18:24:25 +0100

    added an application demo

--- Museum-API/src/main/java/com/dians/museumapi/MuseumApiApplication.java
+++ Museum-API/src/main/java/com/dians/museumapi/MuseumApiApplication.java
@@ -3,13 +3,12 @@ package com.dians.museumapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan
 @SpringBootApplication
 public class MuseumApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MuseumApiApplication.class, args);
     }
-
 }
