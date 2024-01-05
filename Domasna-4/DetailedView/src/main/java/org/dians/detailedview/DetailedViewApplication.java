fbaf5380da4aeecb69d1390a932cbaa4cb6b33d5
David Krstevski <david.krstevski@students.finki.ukim.mk>
2024-01-05 06:19:39 +0100

    created a detailed view microservice

---
+++ Domasna-4/DetailedView/src/main/java/org/dians/detailedview/DetailedViewApplication.java
@@ -0,0 +1,13 @@
+package org.dians.detailedview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailedViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailedViewApplication.class, args);
+    }
+
+}
---
+++ Domasna-4/DetailedView/src/test/java/org/dians/detailedview/DetailedViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dians.detailedview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailedViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
